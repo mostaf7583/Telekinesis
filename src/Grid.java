d880e031bd5bfc395bca202f6c1b2155a0131bbf
mostaf7583 <73074293+mostaf7583@users.noreply.github.com>
2023-07-19 14:59:35 +0300

    generation mehtod

--- src/Grid.java
+++ src/Grid.java
@@ -1,3 +1,5 @@
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Random;
 
 public class Grid {
@@ -19,65 +21,138 @@ public class Grid {
 
         for (int row = 0; row < grid.length; row++) {
             for (int col = 0; col <  grid[row].length; col++) {
-                if (row == agentRow && (col == agentCol || col == agentCol + 1)) {
-                    System.out.print(" A ");  // Print agent marker
-                } else {
+            
                     switch (grid[row][col]) {
                         case 0:
                             System.out.print(" - ");  // Print blank cell marker
                             break;
                         case 1:
-                            System.out.print(" X ");  // Print agent body marker
+                            System.out.print(" A ");  // Print agent body marker
                             break;
                         case 2:
                             System.out.print(" F ");  // Print furniture marker
                             break;
+                        case 3:
+                            System.out.print(" G ");  // Print goal marker
+                            break;
                     }
-                }
+                
             }
             System.out.println();  // Move to the next line after each row
         }
     }
     // grid generation
-     private static final int MIN_GRID_SIZE = 4;
-    private static final int BLANK = 0;
-    private static final int FURNITURE = 2;
 
-    public static Grid generateGrid(int n, int m, int numFurniture) {
-        if (n < MIN_GRID_SIZE || m < MIN_GRID_SIZE) {
-            throw new IllegalArgumentException("Grid dimensions must be at least 4x4.");
-        }
 
-        if (numFurniture <= 0) {
-            throw new IllegalArgumentException("Number of furniture must be positive.");
-        }
+    public static Grid generateGrid(int n, int m,int numFurniture) { // n rows, m columns
+       
 
         int[][] grid = new int[n][m];
         int agentRow = 1;  // The agent is in the second row from the top
-
-        // Place furniture randomly
+    
+        // Place the agent in the first two cells of the agent row
+        grid[agentRow][0] = 1;
+        grid[agentRow][1] = 1;
+    
+        // Place the goal (G) at the end of the second row
+        grid[1][m - 1] = 3;
+    
+        // Generate furniture and blanks randomly
         Random random = new Random();
-        int remainingFurniture = numFurniture;
-        while (remainingFurniture > 0) {
+        int furnitureCount = 0;
+    
+        while (furnitureCount < numFurniture) {
             int row = random.nextInt(n);
             int col = random.nextInt(m);
-
+    
             if (row == agentRow && col < 2) {
                 // Skip agent's position
                 continue;
             }
-
-            if (grid[row][col] == BLANK) {
-                grid[row][col] = FURNITURE;
-                remainingFurniture--;
+    
+            if (grid[row][col] == 0 && grid[row][col] != 3) {
+                int furnitureLength = random.nextInt(2) + 2; // Generate furniture of length 2 or 3
+    
+                // Randomly select the orientation (horizontal or vertical)
+                boolean isVertical = random.nextBoolean();
+    
+                if (isVertical && row + furnitureLength <= n) {
+                    // Check if there is enough space to place vertical furniture
+                    boolean canPlaceFurniture = true;
+                    for (int i = 0; i < furnitureLength; i++) {
+                        if (grid[row + i][col] != 0 || grid[row + i][col] == 3) {
+                            canPlaceFurniture = false;
+                            break;
+                        }
+                    }
+                    if (canPlaceFurniture) {
+                        for (int i = 0; i < furnitureLength; i++) {
+                            grid[row + i][col] = 2; // Furniture marker
+                        }
+                        furnitureCount++;
+                    }
+                } else if (!isVertical && col + furnitureLength <= m) {
+                    // Check if there is enough space to place horizontal furniture
+                    boolean canPlaceFurniture = true;
+                    for (int i = 0; i < furnitureLength; i++) {
+                        if (grid[row][col + i] != 0 || grid[row][col + i] == 3) {
+                            canPlaceFurniture = false;
+                            break;
+                        }
+                    }
+                    if (canPlaceFurniture) {
+                        for (int i = 0; i < furnitureLength; i++) {
+                            grid[row][col + i] = 2; // Furniture marker
+                        }
+                        furnitureCount++;
+                    }
+                }
             }
         }
-
-        return new Grid(grid, agentRow, 0);  // Adjust agentCol as needed
+    
+        return new Grid(grid, agentRow, 0); // Adjust agentCol as needed
     }
+
+
+   public void saveGridToFile(String filename) {
+        try {
+            FileWriter fileWriter = new FileWriter(filename, true);
+            for (int[] row : grid) {
+                for (int cell : row) {
+                    String symbol;
+                    switch (cell) {
+                        case 0:
+                            symbol = "-";
+                            break;
+                        case 1:
+                            symbol = "A";
+                            break;
+                        case 2:
+                            symbol = "F";
+                            break;
+                        case 3:
+                            symbol = "G";
+                            break;
+                        default:
+                            symbol = "?";
+                            break;
+                    }
+                    fileWriter.write(symbol + " ");
+                }
+                fileWriter.write("\n");
+            }
+             fileWriter.write("---------------------\n");
+            fileWriter.close();
+            System.out.println("Grid saved to file: " + filename);
+        } catch (IOException e) {
+            System.err.println("Error while saving the grid to file: " + e.getMessage());
+        }
+    }
+
     public static void main(String[] args) {
-        Grid grid = Grid.generateGrid(5, 5, 5);
+        Grid grid = Grid.generateGrid(5, 5,2);
         grid.printGrid();
+        grid.saveGridToFile("grid.txt");
     }
 
     
