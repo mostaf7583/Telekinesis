9b99b3919c9bac8a665866b98f3ee64c27e5e94c
Tasneem Elghobashy <46913410+tasneem57@users.noreply.github.com>
2023-07-23 13:00:55 +0000

    Node expand method

--- src/Node.java
+++ src/Node.java
@@ -1,6 +1,6 @@
 public class Node {
     Grid state;
-    
+    arrayList<Grid> leaves = new arrayList<Grid>();
     public boolean goaltest(){
         int agentCol = state.agentCol;
         for(int i= agentCol+2;i<state.grid[1].length;i++){
@@ -10,7 +10,17 @@ public class Node {
     }
   
     public void expand(){
-        
+        for(int i=0;i<state.furnitures.size();i++){
+            Furniture f = state.furnitures.get(i);
+            if(f.orientation == 'V'){
+                leaves.add(state.moveUp(f));
+                leaves.add(state.moveDown(f));
+            }
+            else if(f.orientation == 'H'){
+                leaves.add(state.moveRight(f));
+                leaves.add(state.moveLeft(f));
+            }
+        }
     }
 
 }
