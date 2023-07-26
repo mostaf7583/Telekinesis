fc06f22fd154b24139cab2b4ab113fd308e6fbbe
mostaf7583 <73074293+mostaf7583@users.noreply.github.com>
2023-07-26 16:35:56 +0300

    depth first search

--- src/Search.java
+++ src/Search.java
@@ -1,5 +1,8 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Search {
     public Solution breadthFirst(Node root) {
@@ -66,5 +69,35 @@ public class Search {
             }
         }
         return new Solution(expanSequence,queue,root,false);
+    }
+   public Solution breadthLimited(Node root, int depthLimit) {
+        Queue<Node> queue = new LinkedList<>();
+        HashSet<String> uniqueStates = new HashSet<>();
+        ArrayList<Node> expandSequence = new ArrayList<>();
+        root.key = 1;
+        long index = 1;
+        queue.add(root);
+        uniqueStates.add(root.strState);
+        root.expand();
+        root.cost = 0;
+        while (!queue.isEmpty()) {
+            Node node = queue.remove();
+            expandSequence.add(node);
+            if (node.goaltest()) {
+                return new Solution(expandSequence, new ArrayList<>(queue), node, true);
+            }
+            if (node.getDepth() < depthLimit) {
+                for (Grid child : node.leaves) {
+                    Node newNode = new Node(child);
+                    if (!uniqueStates.contains(newNode.strState)) {
+                        newNode.expand();
+                        queue.add(newNode);
+                        newNode.key = ++index;
+                        uniqueStates.add(newNode.strState);
+                    }
+                }
+            }
+        }
+        return new Solution(expandSequence, new ArrayList<>(queue), root, false);
     }
 }
